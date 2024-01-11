7487943f631ba0b51c4bcfd9bd4ec0ddfc45f594
Antonio Manno <antonio.manno.7@gmail.com>
2024-01-11 13:07:28 +0000

    create questions array in js file

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+// Questions
+
+let questions = [
+
+    {
+        title: 'What is the correct HTML tag for creating a hyperlink?',
+        answers: ['<link>', '<href>', '<a>', '<url>'],
+        correct: '<a>'
+    },
+
+    {
+        title: 'How can you select an element with id "example" in CSS?',
+        answers: ['#example', ' .example', ' element example', 'select.example'],
+        correct: '#example'
+    },
+
+    {
+        title: 'What keyword is used to declare a variable in JavaScript?',
+        answers: ['v', 'var', 'variable', 'const'],
+        correct: 'var'
+    },
+
+    {
+        title: 'Which HTML tag is used for creating an unordered list?',
+        answers: ['<list>', '<ul>', '<ol>', '<li>'],
+        correct: '<ul>'
+    },
+
+    {
+        title: 'How do you set the background color of an element in CSS?',
+        answers: ['color: background;', 'bg-color: #RRGGBB;', 'background-color: #RRGGBB;', 'set-background: #RRGGBB;'],
+        correct: 'background-color: #RRGGBB;'
+    },
+    
+    {
+        title: 'What does the "=== " operator do in JavaScript?',
+        answers: ['Assigns a value', 'Compares values and types', 'Concatenates strings', 'Performs division'],
+        correct: 'Compares values and types'
+    },
+    
+    {
+        title: 'What is the correct HTML tag for inserting a line break?',
+        answers: ['<br>', '<lb>', '<break>', '<newline>'],
+        correct: '<br>'
+    },
+    
+    {
+        title: 'How can you apply styles to an element when a user hovers over it?',
+        answers: [':hover', '.hover', 'hover:style', 'onHover'],
+        correct: ':hover'
+    },
+
+    {
+        title: 'What is the purpose of the "document.getElementById()" function in JavaScript?',
+        answers: ['Gets a class element', 'Gets an element by its tag name', 'Gets an element by its id', 'Gets an element by its name'],
+        correct: 'Gets an element by its id'
+    },
+    
+    {
+        title: 'Which tag is used to define an external JavaScript file?',
+        answers: ['<js>', '<script>', '<javascript>', '<external>'],
+        correct: '<script>'
+    },
+
+]
